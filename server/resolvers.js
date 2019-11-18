cf9ae2ec29282be3101e63df9e5293013ba08075
acsrikar279 <acsrikar@gmail.com>
2019-11-18 22:42:34 +0530

    rewritten requests

--- server/resolvers.js
+++ server/resolvers.js
@@ -7,11 +7,15 @@ const Query = {
 };
 
 const Mutation = {
-    createJob: (root, {input}) => {
-        const id =  db.jobs.create(input);
+    createJob: (root, {input}, {user}) => {
+        //check user auth
+        if(!user){
+            throw new Error('Unauthorized Action');
+        }
+        const id =  db.jobs.create({...input, companyId: user.companyId});
         return db.jobs.get(id);
     }
-}
+};
 
 const Job = {
     company: (job) => db.companies.get(job.companyId)
